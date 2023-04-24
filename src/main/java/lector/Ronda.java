9b3b492b91fad5ca0fee4b2cf1fec2f8dd39547c
Aldos <aldosalerno@gmail.com>
2023-04-24 20:35:18 -0300

    Update Ronda.java

--- src/main/java/lector/Ronda.java
+++ src/main/java/lector/Ronda.java
@@ -1,31 +1,52 @@
 package lector;
 
+import java.util.ArrayList;
+
 public class Ronda 
 {
-	public String nro;
 	public Partido partido1;
 	public Partido partido2;
 	public Pronostico pronostico1;
 	public Pronostico pronostico2;
 	public int puntosFinales;
+	public String  nro;
 	
-	public Ronda (Partido nPartido1, Partido nPartido2, Pronostico nPronostico1, Pronostico nPronostico2) 
+	public Ronda (Partido partido1, Partido partido2, Pronostico pronostico1, Pronostico pronostico2) 
 	{
 		nro = "Uno";
-		partido1 = nPartido1;
-		partido2 = nPartido2;
-		pronostico1 = nPronostico1;
-		pronostico2 = nPronostico2;
+		this.partido1 = partido1;
+		this.partido2 = partido2;
+		this.pronostico1 = pronostico1;
+		this.pronostico2 = pronostico2;
+		
 		puntosFinales = pronostico1.getPuntos() + pronostico2.getPuntos();
 		
 		System.out.println("Ronda Creada:");
 		System.out.println("	Ronda Nro: " + nro);
-		System.out.println("	Primer Partido Id: " + partido1.getPartidoId());
+		System.out.println("	Primer  Partido Id: " + partido1.getPartidoId());
 		System.out.println("	Segundo Partido Id: " + partido2.getPartidoId());
 		System.out.println(" ");
 		System.out.println("	Puntos totales de Maria: " + puntosFinales);
-		System.out.println(" ");
 		
 	}
+	
+	public Ronda (Partido partido1, Partido partido2, ArrayList<Pronostico> pronosticosLeidos) 
+	{
+		nro = "Uno";
+		this.partido1 = partido1;
+		this.partido2 = partido2;
+	    this.pronostico1 = pronosticosLeidos.get(0);
+	    this.pronostico2 = pronosticosLeidos.get(1);
+		 
+		puntosFinales = this.pronostico1.getPuntos() + this.pronostico2.getPuntos();
+		
+		System.out.println("Ronda Creada:");
+		System.out.println("	Ronda Nro: " + nro);
+		System.out.println("	Primer  Partido Id: " + partido1.getPartidoId());
+		System.out.println("	Segundo Partido Id: " + partido2.getPartidoId());
+		System.out.println(" ");
+		System.out.println("	Puntos totales de Maria: " + puntosFinales);
+	     
+    }
 	
 }
