be41275e43f713b9f78700ea94b970bc9abd9d83
Aldos <aldosalerno@gmail.com>
2023-04-24 20:34:15 -0300

    Create ConectorSQL.java

---
+++ src/main/java/conexion/SQL/ConectorSQL.java
@@ -0,0 +1,12 @@
+package conexion.SQL;
+
+public class ConectorSQL {
+
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/Apuestas";
+
+    // Credenciales de acceso
+    public static final String USER = "root";
+    public static final String PASS = "root";
+	
+}
